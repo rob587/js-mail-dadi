60e5d7352fd485b279ecd2fc589d438bd708d552
rob587 <robertocamm02@yahoo.com>
2025-07-04 15:51:20 +0200

    lista email creata

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Mail
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Si può fare? Certo che si basta ragionare un po’.
+// Nota: 
+// Non è necessario provvedere alla validazione delle email
+
+//dichiarazione variabile email ergo creare un array di email//
+
+const emailList = ["Email1, Email2, Email3, Email4, Email5, Email6, Email7, Email8,"]
+
+//prompt per chiedere email all'utente//
+
+
+
+//controllare che sia in lista
+
+
+//stampare messaggio sull'esito di controllo//
